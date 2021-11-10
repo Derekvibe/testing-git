13e621814d1b7a171935a5a9420e2e163b650c00
Derek Emmanuel <derekemmanuel99@gmail.com>
2021-11-10 14:59:45 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var arr = ["Vincent", "Ihechikara", 300, true];
+
+//Loops
+
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (i in arr) {
+//   console.log(arr[i]);
+// }
+
+// arr.map((items)=>{
+//     console.log(items)
+// })
+
+// arr.forEach((i)=>{
+//     console.log(i)
+// })
+
+// var secondArr = {
+//     name: "Vincent",
+//     author: "Derek",
+//     dob: "1967"
+// }
+
+
+
+// var level = 400
+
+// function add(a,b){
+//     return a + b;
+// }
+
+// var sub = function(a,b){
+//     return a -b;
+// }
+
+// console.log(sub(4,1))
+
+// var nam = ()=>{
+//     alert(`ghcdfchgdd`)
+// }
+
+// var btn = document.getElementById("btn");
+// var content = document.getElementById("content");
+
+// btn.addEventListener("click", ()=>{
+//     let p = document.createElement("p");
+
+//     p.innerText = "I am a paragraph";
+
+//     content.appendChild(p);
+// })
